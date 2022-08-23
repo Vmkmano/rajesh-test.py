2251eaa8a55cd0bf5065155b1d3e7e0c0a8fbc29
Vmkmano <110783276+Vmkmano@users.noreply.github.com>
2022-08-23 00:44:12 -0700

    Update rajesh.py

--- rajesh.py
+++ rajesh.py
@@ -1,1 +1,8 @@
-print("hi vmk")
+cloum=int(input("enter the cloum:"))
+row=int(input("enter the row:"))
+symble=input("enter the sysble:")
+for i in range(cloum):
+    for x in range(row):
+        print(symble,end="")
+    print()
+    
